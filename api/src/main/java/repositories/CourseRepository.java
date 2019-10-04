668b5dbdb6af16dbe96fc158ce8c78a9e5bfbd8e
arraisk <arrais.gbr@gmail.com>
2019-10-04 09:42:34 -0300

    crud2

--- api/src/main/java/repositories/CourseRepository.java
+++ api/src/main/java/repositories/CourseRepository.java
@@ -1,6 +1,8 @@
-package com.example.demo;
+package repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import models.Course;
 
 public interface CourseRepository extends JpaRepository<Course, Long>{
 
