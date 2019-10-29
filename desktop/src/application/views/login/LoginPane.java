1b74780011167844f30d4a9dbe34f73d26b8690a
Luiz Gustavo Oliveira Mariz <luiz.mariz@MJV.net>
2019-10-29 02:34:26 -0300

    added: professor pane and class/students listView

---
+++ desktop/src/application/views/login/LoginPane.java
@@ -0,0 +1,33 @@
+package application.views.login;
+
+import application.Router;
+import application.views.register.RegisterPane;
+import com.jfoenix.controls.JFXButton;
+import javafx.fxml.FXML;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.Pane;
+
+import java.io.IOException;
+
+public class LoginPane {
+    private String userId = "WeDoNotHaveLoginYet";
+    // private Http req;
+
+    @FXML private JFXButton loginBtn;
+    @FXML private Pane loginPane;
+    @FXML private TextField loginCpfField;
+    @FXML private PasswordField loginPasswordField;
+
+    @FXML
+    public void handleLogin () throws IOException {
+        String role = loginCpfField.getText();
+        if (role.equals("admin")) {
+            RegisterPane paneController = Router.<RegisterPane>toView("views/register/registerPane.fxml");
+            paneController.setUser(userId);
+        }
+        if (role.equals("professor")) {
+            Router.toView("views/professor/professorDashboardPane.fxml");
+        }
+    }
+}
---
+++ desktop/src/application/views/professor/Student.java
@@ -0,0 +1,67 @@
+package application.views.professor;
+
+import com.jfoenix.controls.datamodels.treetable.RecursiveTreeObject;
+import javafx.beans.property.SimpleFloatProperty;
+import javafx.beans.property.SimpleIntegerProperty;
+import javafx.beans.property.SimpleStringProperty;
+
+public class Student extends RecursiveTreeObject<Student> {
+    private SimpleStringProperty dre, nome;
+    private SimpleIntegerProperty faltas;
+    private SimpleFloatProperty media;
+
+    public String getDre() {
+        return dre.get();
+    }
+
+    public SimpleStringProperty dreProperty() {
+        return dre;
+    }
+
+    public void setDre(String dre) {
+        this.dre.set(dre);
+    }
+
+    public String getNome() {
+        return nome.get();
+    }
+
+    public SimpleStringProperty nomeProperty() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome.set(nome);
+    }
+
+    public int getFaltas() {
+        return faltas.get();
+    }
+
+    public SimpleIntegerProperty faltasProperty() {
+        return faltas;
+    }
+
+    public void setFaltas(int faltas) {
+        this.faltas.set(faltas);
+    }
+
+    public float getMedia() {
+        return media.get();
+    }
+
+    public SimpleFloatProperty mediaProperty() {
+        return media;
+    }
+
+    public void setMedia(float media) {
+        this.media.set(media);
+    }
+
+    public Student(String dre, String nome, Integer faltas, Float media) {
+        this.dre = new SimpleStringProperty(dre);
+        this.nome = new SimpleStringProperty(nome);
+        this.faltas = new SimpleIntegerProperty(faltas);
+        this.media = new SimpleFloatProperty(media);
+    }
+}
