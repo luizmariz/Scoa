0024660700c8256942c9897203e544ed1c7fba70
Luiz <37978726+luizmariz@users.noreply.github.com>
2019-11-22 11:52:47 -0300

    Add class screen and stack navigation

--- scoa/src/main/java/scoa/desktop/views/ProfessorDashboardPane.java
+++ scoa/src/main/java/scoa/desktop/views/ProfessorDashboardPane.java
@@ -1,5 +1,7 @@
 package scoa.desktop.views;
 
+import com.jfoenix.controls.JFXButton;
+import javafx.scene.text.Text;
 import scoa.desktop.Router;
 import com.jfoenix.controls.JFXListView;
 import javafx.collections.FXCollections;
@@ -12,16 +14,57 @@ import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.Pane;
 
 import java.io.IOException;
+import java.lang.reflect.Array;
+import java.util.ArrayDeque;
+import java.util.Deque;
 
 public class ProfessorDashboardPane {
 
-    @FXML private JFXListView classesList;
+    @FXML private JFXListView disciplineList, classList;
     @FXML private TableView<Student> studentTable;
     @FXML private TableColumn dreCol, nomeCol, faltasCol, mediaCol;
-    @FXML private Pane classPane;
+    @FXML private Pane classPane, profClassPane, profDisciplinePane;
+    @FXML private JFXButton backBtn;
+    @FXML private Text classCode, disciplineCode;
 
-    private void handleSelectClass (MouseEvent event) {
-        System.out.println("clicked on " + classesList.getSelectionModel().getSelectedItem());
+    Deque<Pane> stack = new ArrayDeque<Pane>();
+
+    @FXML
+    private void handleBack() {
+        Pane pane = stack.pop();
+        pane.toFront();
+
+        if (stack.isEmpty()) {
+            backBtn.setVisible(false);
+        }
+    }
+
+    private void setDisciplineList(String[] arr) {
+        for (String s : arr) {
+            disciplineList.getItems().add(new Label(s));
+        }
+    }
+
+    private void setClassList(String[] arr) {
+        for (String s : arr) {
+            classList.getItems().add(new Label(s));
+        }
+    }
+
+    private void handleSelectDiscipline(MouseEvent event) {
+        String selected = ((Label)disciplineList.getSelectionModel().getSelectedItem()).getText();
+        System.out.println("clicked on " + selected);
+        disciplineCode.setText(selected);
+        profClassPane.toFront();
+        stack.push(profDisciplinePane);
+        backBtn.setVisible(true);
+        setClassList(new String[]{"Turma 202", "Turma 301"});
+    }
+
+    private void handleSelectClass(MouseEvent event) {
+        String selected = ((Label)classList.getSelectionModel().getSelectedItem()).getText();
+        System.out.println("clicked on " + selected);
+        classCode.setText(selected);
 
         ObservableList<Student> data = FXCollections.observableArrayList(
                 new Student("1920349", "Ciclano", 0, (float) 0),
@@ -30,20 +73,22 @@ public class ProfessorDashboardPane {
 
         studentTable.setItems(data);
         classPane.toFront();
-    }
-
-    private void setClassesList (String[] arr) {
-        for (String s : arr) {
-            classesList.getItems().add(new Label(s));
-        }
+        stack.push(profClassPane);
     }
 
     @FXML
-    private void initialize () {
-        String[] sArr = {"MAB489", "FES7219","MAB489", "FES7219","MAB489", "FES7219","MAB489", "FES7219","MAB489", "FES7219","MAB489", "FES7219","MAB489", "FES7219","MAB489", "FES7219","MAB489", "FES7219"};
-        setClassesList(sArr);
+    private void initialize() {
+        String[] sArr = {"MAB489", "FES7219","MAB489", "FES7219","MAB489", "FES7219"};
+        setDisciplineList(sArr);
 
-        classesList.setOnMouseClicked(new EventHandler<MouseEvent>() {
+        disciplineList.setOnMouseClicked(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent event) {
+                handleSelectDiscipline(event);
+            }
+        });
+
+        classList.setOnMouseClicked(new EventHandler<MouseEvent>() {
             @Override
             public void handle(MouseEvent event) {
                 handleSelectClass(event);
@@ -57,7 +102,7 @@ public class ProfessorDashboardPane {
     }
 
     @FXML
-    public void handleLogout () throws IOException {
+    public void handleLogout() throws IOException {
         Router.toView("views/loginPane.fxml");
     }
 }
